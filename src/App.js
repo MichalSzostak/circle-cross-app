aaa22b3762303125b885cac8675d27e41f10c644
michalszostak <michal.szostak56@gmail.com>
2023-02-28 16:30:18 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import rock from './images/rock.png';
+import paper from './images/paper.png';
+import scissors from './images/scissors.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Choose your move </h1>
       </header>
+      <div className='move-pick-container'>
+          <button className='move-pick-button'><img src={rock} className='buttonPic' alt="rock" /> Rock</button>
+          <button className='move-pick-button'><img src={paper} className='buttonPic' alt="paper" /></button>
+          <button className='move-pick-button'><img src={scissors}  className='buttonPic'  alt="scissors" /></button>
+        </div>
     </div>
   );
 }
